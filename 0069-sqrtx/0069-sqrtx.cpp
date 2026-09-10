d22b342a3795add6855152428f211da7c52f52b9
Mr AJ <ajaykumardbg2028@gmail.com>
2026-09-10 23:08:58 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.33%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        if(x < 2) {
+            return x;
+        }
+
+        int low = 1;
+        int high = x / 2;
+        int ans = 0;
+
+        while(low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            // mid * mid <= x without overflow
+            if(mid <= x / mid) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
